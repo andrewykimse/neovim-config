a51250734fe78395d825aaffa7f061c248409979
Andrew Kim <akim7@anduril.com>
2026-04-15 18:47:12 -0700

    more animations

---
+++ nvim/lua/plugins/animate.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.animate",
+  version = "*",
+  event = "VeryLazy",
+  opts = {
+    cursor = { enable = true },
+    scroll = { enable = true },
+    resize = { enable = true },
+    open = { enable = false },
+    close = { enable = false },
+  },
+}
