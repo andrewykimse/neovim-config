19eeab6d97aba2dac193e5763d21579bab587812
Andrew Kim <akim7@anduril.com>
2026-04-16 16:19:20 -0700

    added leader q for quitting current open buffer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,3 +5,6 @@ vim.keymap.set("n", "<A-h>", "<C-w><", { desc = "Resize left" })
 vim.keymap.set("n", "<A-j>", "<C-w>-", { desc = "Resize down" })
 vim.keymap.set("n", "<A-k>", "<C-w>+", { desc = "Resize up" })
 vim.keymap.set("n", "<A-l>", "<C-w>>", { desc = "Resize right" })
+
+-- Buffer management
+vim.keymap.set("n", "<leader>q", "<cmd>bd<CR>", { desc = "Close current buffer" })
