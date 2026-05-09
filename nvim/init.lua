bb1544b426f4596b9dc15bba6b39dfc812d65e98
andrewkim <andrewykimse@gmail.com>
2026-05-08 20:45:50 -0700

    reverted dual number attempt

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.o.autoread = true
 vim.g.lazyvim_rocks = false
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum} │%l "
+vim.cmd("set number relativenumber")
 require("config.options")
 require("config.keymaps")
 require("config.lazy") -- where you set up lazy and your plugins
