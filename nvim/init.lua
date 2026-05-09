5fe3f932d1b5916b34f31338cd6e4245a14d9099
andrewkim <andrewykimse@gmail.com>
2026-05-08 20:43:41 -0700

    added dual line number mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,9 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.o.autoread = true
 vim.g.lazyvim_rocks = false
-vim.cmd("set number relativenumber")
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum} │%l "
 require("config.options")
 require("config.keymaps")
 require("config.lazy") -- where you set up lazy and your plugins
