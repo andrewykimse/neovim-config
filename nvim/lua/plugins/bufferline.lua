d60df4af1d30b1ad1536baf812322c6f9a9d7d56
andrewkim <andrewykimse@gmail.com>
2026-05-20 21:39:17 -0700

    buffer navigation reverted to hl

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,26 +1,26 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
-  opts = {
-    options = {
-      diagnostics = "nvim_lsp",
-      offsets = {
-        {
-          filetype = "NvimTree",
-          text = "File Explorer",
-          highlight = "Directory",
-          separator = true,
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    opts = {
+        options = {
+            diagnostics = "nvim_lsp",
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "File Explorer",
+                    highlight = "Directory",
+                    separator = true,
+                },
+            },
+            separator_style = "slant",
         },
-      },
-      separator_style = "slant",
     },
-  },
-  keys = {
-    { "<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick buffer" },
-    { "<leader>bc", "<cmd>BufferLinePickClose<cr>", desc = "Pick close buffer" },
-    { "<S-j>", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
-    { "<S-k>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-  },
+    keys = {
+        { "<leader>bp", "<cmd>BufferLinePick<cr>",      desc = "Pick buffer" },
+        { "<leader>bc", "<cmd>BufferLinePickClose<cr>", desc = "Pick close buffer" },
+        { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
+        { "<S-l>",      "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    },
 }
