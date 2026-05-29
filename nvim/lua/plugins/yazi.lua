8592139af42f536e6c542f404f50ad7a90aed12b
Andrew Kim <andrewykimse@gmail.com>
2026-05-28 20:19:44 -0700

    yazi changes added telescope grepping

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -9,5 +9,13 @@ return {
     open_for_directories = false, -- keep oil/nvim-tree as directory handlers
     floating_window_scaling_factor = 0.9,
     yazi_floating_window_border = "rounded",
+    integrations = {
+      grep_in_directory = function(directory)
+        require("telescope.builtin").live_grep({ cwd = directory })
+      end,
+      grep_in_selected_files = function(selected_files)
+        require("telescope.builtin").live_grep({ search_dirs = selected_files })
+      end,
+    },
   },
 }
